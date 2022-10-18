d0be95b8bb93b2f3bbf0823ed348736f7427b955
TSRodgers <tyler.rodgers74@gmail.com>
2022-10-18 19:19:04 -0400

    set up graphql queries

---
+++ server/server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+// import ApolloServer
+const { ApolloServer } = require('apollo-server-express');
+
+// import our typeDefs and resolvers
+const { typeDefs, resolvers } = require('./schemas');
+const db = require('./config/connection');
+
+const PORT = process.env.PORT || 3001;
+// create a new Apollo server and pass in our schema data
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
+
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// create a new instance of an Apollor server with the GraphQL schema
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+  //integrate out Apollo server with the Express application as middleware
+  server.applyMiddleware({ app });
+
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+    });
+  });  
+}
+
+// call the async function to start the server
+startApolloServer(typeDefs, resolvers);
