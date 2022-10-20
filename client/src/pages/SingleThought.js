d1c5d4179f81965bb6c0f414b31b9493aed072e8
TSRodgers <tyler.rodgers74@gmail.com>
2022-10-20 14:47:54 -0400

    implemented addThought and addReaction

--- client/src/pages/SingleThought.js
+++ client/src/pages/SingleThought.js
@@ -5,6 +5,8 @@ import ReactionList from '../components/ReactionList';
 
 import { useQuery } from '@apollo/client';
 import { QUERY_THOUGHT } from '../utils/queries';
+import auth from '../utils/auth';
+import ReactionForm from '../components/ReactionForm';
 
 const SingleThought = (props) => {
   const { id: thoughtId } = useParams();
@@ -36,6 +38,7 @@ const SingleThought = (props) => {
       {thought.reactionCount > 0 && (
         <ReactionList reactions={thought.reactions} />
       )}
+      {auth.loggedIn() && <ReactionForm thoughtId={thought._id}/>}
     </div>
   );
 };
