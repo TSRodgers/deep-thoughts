1a529f86155219e370a3fd6f65d92bc97ba8af85
TSRodgers <tyler.rodgers74@gmail.com>
2022-10-20 12:01:49 -0400

    implemented grapghql queries and navigation for other pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Header from './components/Header';
 import Footer from './components/Footer';
 
+import Login from './pages/Login';
+import NoMatch from './pages/NoMatch';
+import SingleThought from './pages/SingleThought';
+import Profile from './pages/Profile';
+import Signup from './pages/Signup';
 import Home from './pages/Home';
 
 const httpLink = createHttpLink({
@@ -18,13 +24,40 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
-      <div className='flex-column justify-flex-start min-100-vh'>
-        <Header />
-        <div className='container'>
-          <Home />
+     <Router>
+        <div className='flex-column justify-flex-start min-100-vh'>
+          <Header />
+          <div className='container'>
+            <Routes>
+              <Route
+                path='/'
+                element={<Home/>}
+              />
+              <Route
+                path='/login'
+                element={<Login/>}
+              />
+              <Route
+                path='/signup'
+                element={<Signup/>}
+              />
+              <Route path='/profile'>
+                <Route path=':username' element={<Profile/>} />
+                <Route path='' element={<Profile/>} />
+              </Route>
+              <Route
+                path='/thought/:id'
+                element={<SingleThought/>}
+              />
+              <Route
+                path='*'
+                element={<NoMatch/>}
+              />
+            </Routes>
+          </div>
+          <Footer />
         </div>
-        <Footer />
-      </div>
+     </Router>
     </ApolloProvider>
   );
 }
--- client/src/components/ThoughtList/index.js
+++ client/src/components/ThoughtList/index.js
@@ -1,29 +1,38 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const ThoughtList = ({ thoughts, title }) => {
   if (!thoughts.length) {
-    return <h3>No Thoughts Yet</h3>
+    return <h3>No Thoughts Yet</h3>;
   }
 
   return (
     <div>
       <h3>{title}</h3>
       {thoughts &&
-      thoughts.map(thought => (
-        <div key={thought._id} className='card mb-3'>
-          <p className='card-header'>
-            {thought.username}
-            thought on {thought.createdAt}
-          </p>
-          <div className='card-body'>
-            <p>{thought.thoughtText}</p>
-            <p className='mb-0'>
-              Reactions: {thought.reactionCount} || Click to{' '}
-              {thought.reactionCount ? 'see' : 'start'} the discussion!
+        thoughts.map(thought => (
+          <div key={thought._id} className="card mb-3">
+            <p className="card-header">
+              <Link
+                to={`/profile/${thought.username}`}
+                style={{ fontWeight: 700 }}
+                className="text-light"
+              >
+                {thought.username}
+              </Link>{' '}
+              thought on {thought.createdAt}
             </p>
+            <div className="card-body">
+              <Link to={`/thought/${thought._id}`}>
+                <p>{thought.thoughtText}</p>
+                <p className="mb-0">
+                  Reactions: {thought.reactionCount} || Click to{' '}
+                  {thought.reactionCount ? 'see' : 'start'} the discussion!
+                </p>
+              </Link>
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
     </div>
   );
 };
